4bdfdcb75ab3a36954c37ed5f7021a1eefe28ead
talberadt <tal.beradt@mail.huji.ac.il>
2022-04-07 23:35:09 +0300

    //

--- app/src/main/java/com/huji/hackathon/paperw8/FileListActivity.java
+++ app/src/main/java/com/huji/hackathon/paperw8/FileListActivity.java
@@ -1,8 +1,10 @@
 package com.huji.hackathon.paperw8;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class FileListActivity extends AppCompatActivity {
 
@@ -11,6 +13,10 @@ public class FileListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_list);
 
-//        RecyclerView recyclerView = findViewById(R.id.)
+        RecyclerView recyclerView = findViewById(R.id.recycleView);
+        TextView noFilesTextView = findViewById(R.id.noFilesTextView);
+
+        String path = getIntent().getStringExtra("path");
+//        File root = new File(path)
     }
 }
